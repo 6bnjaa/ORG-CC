ea301bc17d43fc0090a5a98a0650e70996c8c240
6bnjaa <benjaitahonorato@gmail.com>
2024-07-09 18:39:38 -0400

    User

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,4 @@
-import io from 'socket.io-client'; 
-
-const socket = io();
+const socket = io(); // Establecer conexión con el servidor socket.io
 
 new Vue({
     el: '#app',
@@ -13,12 +11,8 @@ new Vue({
         showInitialBalanceInput: false
     },
     created() {
-        // Cargar datos almacenados localmente al iniciar la aplicación
         this.loadData();
-
-        // Escuchar el evento 'updateData' del servidor
         socket.on('updateData', (data) => {
-            // Actualizar los datos en Vue.js cuando se recibe el evento del servidor
             this.accounts = data;
         });
     },
@@ -38,65 +32,46 @@ new Vue({
                 return;
             }
 
-            // Actualizar el balance y registrar la transacción
             account.balance -= account.amount;
             account.transactions.push({ amount: account.amount, description: account.description });
-
-            // Limpiar campos después de la transacción
             account.amount = null;
             account.description = '';
-
-            // Guardar los datos actualizados localmente
             this.saveData();
-
-            // Emitir el evento 'updateData' al servidor
             socket.emit('updateData', this.accounts);
         },
         getTotalTransactions(transactions) {
-            // Calcular el total de transacciones
             if (!Array.isArray(transactions)) {
                 return 0;
             }
             return transactions.reduce((total, transaction) => total + transaction.amount, 0);
         },
         toggleInitialBalanceInput() {
-            // Mostrar el formulario para actualizar el saldo inicial
             this.showMainContent = false;
             this.showInitialBalanceInput = true;
         },
         updateInitialBalance(account) {
-            // Actualizar el saldo inicial y balance
             if (account.newInitialBalance !== null) {
-                account.balance += account.newInitialBalance; // Sumar al balance existente
-                account.initialBalance += account.newInitialBalance; // Sumar al saldo inicial
+                account.balance += account.newInitialBalance;
+                account.initialBalance += account.newInitialBalance;
                 account.newInitialBalance = null;
-
-                // Ocultar el formulario y mostrar el contenido principal
                 this.showMainContent = true;
                 this.showInitialBalanceInput = false;
-
-                // Guardar los datos actualizados localmente
                 this.saveData();
-
-                // Emitir el evento 'updateData' al servidor
                 socket.emit('updateData', this.accounts);
             } else {
                 alert('Por favor, ingresa una cantidad válida');
             }
         },
         saveData() {
-            // Guardar datos en el almacenamiento local
             localStorage.setItem('accounts', JSON.stringify(this.accounts));
         },
         loadData() {
-            // Cargar datos desde el almacenamiento local al iniciar
             const savedAccounts = localStorage.getItem('accounts');
             if (savedAccounts) {
                 this.accounts = JSON.parse(savedAccounts);
             }
         },
         saveToFile() {
-            // Guardar datos en un archivo de texto
             let data = '';
             this.accounts.forEach(account => {
                 data += `${account.name}:\n`;
@@ -113,20 +88,16 @@ new Vue({
             saveAs(blob, `gastos_${new Date().toISOString().slice(0, 10)}.txt`);
         },
         deleteAllData() {
-            // Eliminar todos los datos
             if (confirm('¿Estás seguro que deseas borrar todos los datos?')) {
                 this.accounts.forEach(account => {
                     account.description = '';
                     account.transactions = [];
                 });
                 this.saveData();
-
-                // Emitir el evento 'updateData' al servidor
                 socket.emit('updateData', this.accounts);
             }
         },
         cancelInitialBalanceUpdate() {
-            // Cancelar la actualización del saldo inicial
             this.showMainContent = true;
             this.showInitialBalanceInput = false;
         }
