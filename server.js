2798fdd89d980791e08d70d6c51415d2f4d79643
6bnjaa <benjaitahonorato@gmail.com>
2024-07-10 20:45:42 -0400

    Up

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        localStorage.setItem('accounts', JSON.stringify(this.accounts));
     });
 
     socket.on('updateData', (data) => {
